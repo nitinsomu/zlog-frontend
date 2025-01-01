3edeb39c25ad5c49dfacf84fbc189222051a9a67
nitinsomu <nitin.somu13@gmail.com>
2025-01-02 00:00:13 +0530

    Code to save cookie in browser

--- blog-website/src/pages/login.jsx
+++ blog-website/src/pages/login.jsx
@@ -1,24 +1,31 @@
 import { useState } from "react"
-
+import {Navigate} from "react-router-dom"
 export default function Login() {
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-
+    const [redirect, setRedirect] = useState(false)
+ 
     async function login(e) {
         e.preventDefault();
         const response = await fetch('http://localhost:5000/login', {
             method: 'POST',
             body: JSON.stringify({username, password}),
-            headers: {'Content-type':'application/json'}            
+            headers: {'Content-type':'application/json'},
+            credentials: 'include'        
         })
 
-        if (response.status === 200) {
-            alert("Login Successful")
+        if (response.ok) {
+            setRedirect(true);
         } else {
             const errorData = await response.json();
-            alert(errorData.error) 
+            alert(errorData.error);
         }
+    }
+    if (redirect) {
+        return (
+            <Navigate to={'/'} />
+        )
     }
     return (
         <div className="login">
