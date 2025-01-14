851c7174973d1ced5a0315233fd27eb9ad3b9600
nitinsomu <nitin.somu13@gmail.com>
2025-01-14 23:36:45 +0530

    Add logout functionality

--- blog-website/src/components/header.jsx
+++ blog-website/src/components/header.jsx
@@ -1,12 +1,46 @@
+import { useEffect, useState } from 'react';
 import './header.css'
 import { Link } from 'react-router-dom';
+
 function Header() {
+    const [username, setUsername] = useState(null);
+    useEffect(() => {
+        fetch('http://localhost:5000/profile', {
+            credentials: 'include',
+        }
+        ).then(response => {
+                response.json().then(userData => {
+                setUsername(userData.username)
+            })
+        }).catch(err => {
+            console.error('Failed to fetch profile:', err);
+        })
+    }, [])
+    
+    function logout() {
+        fetch('http://localhost:5000/logout', {
+            method: 'POST',
+            credentials: 'include'
+        });
+        setUsername(null);
+    }
+
     return (
         <header>
             <Link to="/" className="logo">Zlog</Link>
             <nav>
+                {username && 
+                    (<>
+                        <Link to="/create">Create Post</Link>
+                        <a onClick={logout}>Logout</a>
+                    </>)
+                }
+                {!username &&
+                (<>               
                 <Link to="/login">Login</Link>
                 <Link to="/register">Register</Link>
+                </>)
+                }
             </nav>
         </header>
     )
