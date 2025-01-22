872cb2b8814753d444b20ea90f7b9d2a1e7ac00b
nitinsomu <nitin.somu13@gmail.com>
2025-01-22 23:57:57 +0530

    Added boiler plate for user context

--- blog-website/src/App.jsx
+++ blog-website/src/App.jsx
@@ -5,16 +5,19 @@ import Home from './pages/home'
 import Login from './pages/login'
 import Register from './pages/register'
 import Layout from './components/layout'
+import { UserContext, UserContextProvider } from './UserContext'
 
 function App() {
   return (
-    <Routes>
-      <Route path={"/"} element={<Layout />}>
-        <Route index element={<Home/>} />
-        <Route path={'/login'} element={<Login/>} />
-        <Route path={'/register'} element={<Register/>} />
-      </Route>
-    </Routes>
+    <UserContextProvider>
+      <Routes>
+        <Route path={"/"} element={<Layout />}>
+          <Route index element={<Home/>} />
+          <Route path={'/login'} element={<Login/>} />
+          <Route path={'/register'} element={<Register/>} />
+        </Route>
+      </Routes>
+    </UserContextProvider>
   )
 }
 
